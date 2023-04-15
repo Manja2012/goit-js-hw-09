ca20b8b1af6b1012cd4fc079c4f2123a2066f124
Marianna Demchenko <mdemko2012@gmail.comm>
2023-04-15 12:47:03 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,42 @@
 import Notiflix from 'notiflix';
 
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+     const shouldResolve = Math.random() > 0.3;
+     setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+     }, delay)
+  });
+}
+
+function showPromise(delay, step, amount) {
+  let position = 0;
+  for (let i = 0; i < amount; i += 1) {
+    position += 1;
+    createPromise(position, delay)
+      .then(resolve => {
+        Notiflix.Notify.success(resolve);
+      })
+      .catch(reject => {
+        Notiflix.Notify.failure(reject);
+      });
+    delay = Number(delay) + Number(step);
   }
+}
+
+form.addEventListener('submit', submitBtn);
+
+function submitBtn(e) {
+  e.preventDefault();
+  const delay = form.delay.value;
+  const step = form.step.value;
+  const amount = form.amount.value;
+  showPromise(delay, step, amount);
+  e.currentTarget.reset();
 }
